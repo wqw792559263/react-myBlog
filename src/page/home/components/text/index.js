fb568291fc5bf4e61a7f79e0f930f3419031fcbc
Simon Wang <wqw792559263@gmail.com>
2019-07-16 12:51:12 -0300

    npm

--- src/page/home/components/text/index.js
+++ src/page/home/components/text/index.js
@@ -45,8 +45,8 @@ export default class TextNode extends Component {
     return (
       <div className={homeCss.text}>
         <span className={this.state.span1?homeCss.animate:''}>Hey There</span><br/>
-        <span className={this.state.span2?homeCss.animate:''}>I'm Bruce.Au</span><br/>
-        <span className={this.state.span3?homeCss.animate:''}>( σ'ω')σ<strong>#Skr</strong><i className={this.state.cursor?homeCss.cursor:''}></i></span>
+        <span className={this.state.span2?homeCss.animate:''}>I'm Simon Wang</span><br/>
+        <span className={this.state.span3?homeCss.animate:''}><strong>#Skr</strong><i className={this.state.cursor?homeCss.cursor:''}></i></span>
       </div>
     )
   }
